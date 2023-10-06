7e5d0160c1b6fa1846cc3bcfba501f50c719c839
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-10-06 17:25:53 +0100

    Completed exercise on Liskov Substitution Principle

---
+++ src/liskov.cs
@@ -0,0 +1,56 @@
+// This is an exercise on Liskov Substitution Principle.
+// We were given a code that was not correct, according to the principle.
+
+using System;
+
+namespace Geometry
+{
+    class Polygon
+    {
+        public abstract double GetArea();
+        // Because a SetArea would not be a method easily defined in the Polygon class that would suit both Rectangle and Square, it's better to have an abstract method that the derived classes must implement
+    }
+    class Rectangle : Polygon
+    {
+        private double width;
+        private double height;
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+        public double GetWidth()
+        {
+            return width;
+        }
+
+        public double GetHeight()
+        {
+            return height;
+        }
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+
+    class Square : Polygon
+    {
+        private double width;
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+        public void double GetWidth()
+        {
+            return width;
+        }
+        public override double GetArea()
+        {
+            return width * width;
+        }
+    }
+}
